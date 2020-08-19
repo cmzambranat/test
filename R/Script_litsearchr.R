88f03b37d48f2bca8bb131cbee7a70f1829985f4
Carlos Zambrana-Torrelio <cmzambranat@gmail.com>
2020-08-19 17:04:27 -0400

    cambie la libraria here

--- R/Script_litsearchr.R
+++ R/Script_litsearchr.R
@@ -1,26 +1,27 @@
 #Establecer directorio de trabajo
 
-setwd("D:/Rwork")
+#setwd("D:/Rwork")
 
 #Alternativa para instalar "litsearchr"
 #Se requiere instalar previamente "remotes" y "igraph"
 
 library(remotes)
 library(igraph)
+library(litsearchr)
+library(here)
 
 #Enlance con el repositorio de github por medio de "remotes"
 
-remotes::install_github("luketudge/litsearchr", ref="patch-1")
 
-#Una vez ejecutado el comando anterior apararece un menú: seleccionar 1(All) en la consola y dar enter
-#Puede aparecer un ERROR por la paquetería "backport". Se soluciona eliminando dicha paquetería
+#Una vez ejecutado el comando anterior apararece un men?: seleccionar 1(All) en la consola y dar enter
+#Puede aparecer un ERROR por la paqueter?a "backport". Se soluciona eliminando dicha paqueter?a
 
 
 #Ejecutar comando para activar "litsearchr"
 
-library(litsearchr)
 
-#Ecuaciones de búsqueda inicial para Web of Science (Se pueden utilizar otras bases de datos).
+
+#Ecuaciones de b?squeda inicial para Web of Science (Se pueden utilizar otras bases de datos).
 #En el caso de Web of Science descargar metadatos en formato ".bib"
 
 #Malaria
@@ -61,20 +62,20 @@ library(litsearchr)
 
 #malaria
 
-#Importar metadatos de los artículos
+#Importar metadatos de los art?culos
 
 results <- litsearchr::import_results(file = c("malaria1.bib","malaria2.bib"))
 
-#Remover artículos duplicados con base en los títulos
+#Remover art?culos duplicados con base en los t?tulos
 
 rduplicates <- litsearchr::remove_duplicates(results, field="title", method ="exact")
 
-#La función "cleankeywords" se utiliza cuando las palabras clave no están separadas por el caracter ";".
+#La funci?n "cleankeywords" se utiliza cuando las palabras clave no est?n separadas por el caracter ";".
 #Si el archivo que se utiliza para importar los resultados tiene formato ".bib" no es necesario ejecutarla.
 #cleankeywords <- litsearchr::clean_keywords()
 
 
-#Identificar términos que se repiten en el título y el abstract por medio del método "fakerake"
+#Identificar t?rminos que se repiten en el t?tulo y el abstract por medio del m?todo "fakerake"
 
 fakerake <- litsearchr::extract_terms(text = paste(rduplicates$title,rduplicates$abstract),
                                       method = "fakerake",
@@ -85,7 +86,7 @@ fakerake <- litsearchr::extract_terms(text = paste(rduplicates$title,rduplicates
                                       stopwords = F,
                                       language = "English")
 
-#Identificar términos que se repiten en las keyword con el método "tagged"
+#Identificar t?rminos que se repiten en las keyword con el m?todo "tagged"
 
 tagged <- litsearchr::extract_terms(keywords = rduplicates$keywords,
                                             method = "tagged",
@@ -96,7 +97,7 @@ tagged <- litsearchr::extract_terms(keywords = rduplicates$keywords,
                                             stopwords = NULL,
                                             language = "English")
 
-#Unión de los términos por ambos métodos ("fakerake" y "tagged")
+#Uni?n de los t?rminos por ambos m?todos ("fakerake" y "tagged")
 
 all_terms <- unique(append(tagged, fakerake))
 
@@ -104,7 +105,7 @@ all_terms <- unique(append(tagged, fakerake))
 
 dfmatrix <- litsearchr::create_dfm(elements = paste(rduplicates$title,rduplicates$abstract),
                                    features = all_terms)
-#Crear red de todos los términos
+#Crear red de todos los t?rminos
 
 netw <- litsearchr::create_network(search_dfm = as.matrix(dfmatrix),
                                    min_studies = 3,
@@ -114,30 +115,30 @@ netw <- litsearchr::create_network(search_dfm = as.matrix(dfmatrix),
 
 plot(netw)
 
-#Evaluar la importancia de los términos con base en las propiedades de la red.
-#Ordenar los términos con base en su importancia dada por la fuerza de interacción de los nodos (términos)
+#Evaluar la importancia de los t?rminos con base en las propiedades de la red.
+#Ordenar los t?rminos con base en su importancia dada por la fuerza de interacci?n de los nodos (t?rminos)
 
 importance_terms <- make_importance(netw, imp_method  = "strength")
 
-#Recomendación: Visualizar los términos más importantes
-#Corresponden a las últimas filas del data "importance_terms"
-#Aplicar "tail" para visualizar los 30 términos más importantes de la red.
+#Recomendaci?n: Visualizar los t?rminos m?s importantes
+#Corresponden a las ?ltimas filas del data "importance_terms"
+#Aplicar "tail" para visualizar los 30 t?rminos m?s importantes de la red.
 
 tail(importance_terms, n=30)
 
 #Opcional (Exploratorio):
-#Para identificar términos que aparecen más de dos veces ("ngrams")
-#Para identificar términos que aparecen una sola vez ("unigrams")
+#Para identificar t?rminos que aparecen m?s de dos veces ("ngrams")
+#Para identificar t?rminos que aparecen una sola vez ("unigrams")
 #ngrams <- select_ngrams(netw, imp_method = "strength")
 #unigrams <- select_unigrams(netw, imp_method = "strength")
 
-#Identificar el punto de cambio de la fuerza de los términos
+#Identificar el punto de cambio de la fuerza de los t?rminos
 
 findcutoff <- litsearchr::find_cutoff(netw, method = "cumulative",
                                       percent = 0.8,
                                       imp_method = "strength")
 
-#Pedir el findcutoff (punto de cambio), este valor esta dado por la fuerza de interacción.
+#Pedir el findcutoff (punto de cambio), este valor esta dado por la fuerza de interacci?n.
 
 findcutoff
 
@@ -149,13 +150,13 @@ reducegraph <- litsearchr::reduce_graph(graph = netw, cutoff_strength = 98, imp_
 
 plot(reducegraph)
 
-#Extraer los términos de la red reducida
-#Para tener un df con los términos más importantes de la búsqueda inicial.
-#Estos términos son los más importantes por estar arriba del cuttoff (punto de cambio).
+#Extraer los t?rminos de la red reducida
+#Para tener un df con los t?rminos m?s importantes de la b?squeda inicial.
+#Estos t?rminos son los m?s importantes por estar arriba del cuttoff (punto de cambio).
 
 terms <- litsearchr::get_keywords(reducegraph)
 
-#Guardar los términos en un archivo con formato ".csv"
+#Guardar los t?rminos en un archivo con formato ".csv"
 
 write.csv(terms, "terms_malaria.csv")
 
@@ -173,68 +174,68 @@ colnames(df)[1] <- "group"
 
 colnames(df)[2] <- "term"
 
-#Explorar los términos para tenerlos de referencia.
+#Explorar los t?rminos para tenerlos de referencia.
 
-#Hasta este momento tenemos información suficiente para elegir los términos.
-#Por practicidad, sugerimos utilizar los 30 términos más importantes
+#Hasta este momento tenemos informaci?n suficiente para elegir los t?rminos.
+#Por practicidad, sugerimos utilizar los 30 t?rminos m?s importantes
 #Y trabajar con ellos a partir de este punto.
 
 #Retomar el objeto importance_terms
 
-#Hacer un "tail" de los últimos 30 términos (los 30 términos más importantes, según la red)
+#Hacer un "tail" de los ?ltimos 30 t?rminos (los 30 t?rminos m?s importantes, seg?n la red)
 
 tail(importance_terms, n=30)
 
-#Generar data frame con los 30 términos
+#Generar data frame con los 30 t?rminos
 
 dataterms <- tail(importance_terms, n = 30)
 
-#Guardar archivo ".csv" con los 30 términos más importantes
+#Guardar archivo ".csv" con los 30 t?rminos m?s importantes
 
 write.csv(dataterms, "dataterms.csv")
 
 #IMPORTANTE:
-#En estos 30 términos no apareció ninguno relacionado con la deforestación
+#En estos 30 t?rminos no apareci? ninguno relacionado con la deforestaci?n
 #Buscamos en el dataframe de "importance_terms"
-#Un término relacionado con la deforestación arriba del punto de corte.
-#Para esto exploramos el archivo "terms_malaria.csv" que contiene todos los términos arriba del cuttoff.
+#Un t?rmino relacionado con la deforestaci?n arriba del punto de corte.
+#Para esto exploramos el archivo "terms_malaria.csv" que contiene todos los t?rminos arriba del cuttoff.
 
 filter(importance_terms, nodename == "deforested areas")
 
-#Abrimos el archivo "dataterms.csv" con el término que se agregó y la agrupación por categoría.
-#La agrupación se realizó por medio del método PECO.
-#Para más información del método PECO Consultar el artículo de Eliza Grames.
+#Abrimos el archivo "dataterms.csv" con el t?rmino que se agreg? y la agrupaci?n por categor?a.
+#La agrupaci?n se realiz? por medio del m?todo PECO.
+#Para m?s informaci?n del m?todo PECO Consultar el art?culo de Eliza Grames.
 #Population, Exposition, Control (No aplica en nuestro caso) y Result
 
 df.dataterms <- read.csv("dataterms.csv", header = T)
 
-#Aplicamos un "grep" a los términos que se consideraron "population"
+#Aplicamos un "grep" a los t?rminos que se consideraron "population"
 
 population_terms <- df.dataterms$term[grep("population", df.dataterms$group)]
 
-#Visualizamos los términos "population"
+#Visualizamos los t?rminos "population"
 
 population_terms
 
-#Aplicamos un "grep" a los términos que se consideraron "exposition"
-#En este ejemplo el término que se agrupó en esta categoría fue "deforested areas".
-#Este término fue el que se agregó a la lista de los 30 términos más importantes.
+#Aplicamos un "grep" a los t?rminos que se consideraron "exposition"
+#En este ejemplo el t?rmino que se agrup? en esta categor?a fue "deforested areas".
+#Este t?rmino fue el que se agreg? a la lista de los 30 t?rminos m?s importantes.
 
 exposition_terms <- df.dataterms$term[grep("exposition", df.dataterms$group)]
 
-#Visualizamos los términos "exposition"
+#Visualizamos los t?rminos "exposition"
 
 exposition_terms
 
-#Aplicamos un "grep" a los términos que se consideraron "result"
+#Aplicamos un "grep" a los t?rminos que se consideraron "result"
 
 result_terms <- df.dataterms$term[grep("result", df.dataterms$group)]
 
-#Visualizamos los términos "result"
+#Visualizamos los t?rminos "result"
 
 result_terms
 
-#Unimos todas las categorías
+#Unimos todas las categor?as
 
 population_unique <- unique(append(c("population"), population_terms))
 
@@ -250,11 +251,11 @@ result_unique <- unique(append(c("result"), result_terms))
 
 result_unique
 
-#Enlistamos las tres categorías para poder aplicar la función que da la búsqueda sugerida por "litsearchr"
+#Enlistamos las tres categor?as para poder aplicar la funci?n que da la b?squeda sugerida por "litsearchr"
 
 mysearchterms <- list(population_unique, exposition_unique,result_unique)
 
-#Con la función "write_search" obtenemos la ecuación de búsqueda sugerida por "litsearchr" 
+#Con la funci?n "write_search" obtenemos la ecuaci?n de b?squeda sugerida por "litsearchr" 
 
 systemathic_search <- litsearchr::write_search(groupdata = mysearchterms,
                                                languages = "English",
@@ -264,13 +265,13 @@ systemathic_search <- litsearchr::write_search(groupdata = mysearchterms,
                                                writesearch = F,
                                                verbose = T)
 
-#Guardar la ecuación de búsqueda sugerida por "litsearchr" en un archivo con formato ".txt"
+#Guardar la ecuaci?n de b?squeda sugerida por "litsearchr" en un archivo con formato ".txt"
 
 write.table(systemathic_search, "systemathic_search_malaria.txt")
 
-#Con esto terminanos el work flow de "litsearchr" para realizar búsquedas sistemáticas.
+#Con esto terminanos el work flow de "litsearchr" para realizar b?squedas sistem?ticas.
 #Actualizado al 18/08/2020
 #IMPORTANTE:
-#Cuando abran el archivo ".txt" solo copiar a partir del paréntesis
-#y no olvidar borrar las categorías PECO para buscar en la Web of Science u otra base de datos.
+#Cuando abran el archivo ".txt" solo copiar a partir del par?ntesis
+#y no olvidar borrar las categor?as PECO para buscar en la Web of Science u otra base de datos.
 
